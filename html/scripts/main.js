2c5d4a83a7a7d85dedd8555cc66ce9ebd6ae36c8
Ubuntu <ubuntu@ip-10-0-0-47.ec2.internal>
2015-03-22 20:59:49 +0000

    l/g

--- html/scripts/main.js
+++
@@ -1,7 +0,0 @@
-requirejs.config({
-	urlArgs: "unique=" + (new Date()).getTime(),
-    paths: {
-        "jquery": "//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min",
-    }
-});
-requirejs(["vulcun"]);
